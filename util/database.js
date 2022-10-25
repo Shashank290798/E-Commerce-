b7801ed96937a3934d9c0e1d3a0b3cb9b710a39c
Shashank290798 <shashanknalogal@gmail.com>
2022-10-25 22:54:43 +0530

    task12 commit

--- util/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('project', 'root', 'Shashank@143', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
-
-module.exports = sequelize;
